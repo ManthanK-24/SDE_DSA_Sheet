4d480cc27d87479a73c660dab2d62bd55d02f8b5
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-03-23 11:28:17 +0530

    Created P21)MxConsecutiveLen.cpp

---
+++ Day4_Arrays/P21)MxConsecutiveLen.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        int mxConsecutiveLen = 0;
+        
+        unordered_map<int,bool> checked, present;
+        for(auto x:nums)present[x]=1;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            int x = nums[i];
+            if(!checked[x] && !present[x-1])
+            {
+                int cnt = 0;
+                while(present[x])
+                {
+                    cnt++;
+                    x++;
+                }
+                mxConsecutiveLen = max(mxConsecutiveLen, cnt);
+            }
+        }
+        return mxConsecutiveLen;
+        
+    }
+};
