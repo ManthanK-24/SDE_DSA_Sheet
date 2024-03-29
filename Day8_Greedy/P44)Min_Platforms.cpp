593204f90ea852d9ad7581ea569dc469cf111c24
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-03-29 17:43:47 +0530

    Updated P44)Min_Platforms.cpp

--- Day8_Greedy/P44)Min_Platforms.cpp
+++ Day8_Greedy/P44)Min_Platforms.cpp
@@ -1,3 +1,12 @@
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
 class Solution{
     public:
     //Function to find the minimum number of platforms required at the
@@ -27,3 +36,29 @@ class Solution{
     	return pq.size();
     }
 };
+
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
