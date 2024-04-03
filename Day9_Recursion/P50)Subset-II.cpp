acf5c954fc1ecdafbe62de75064cd73ed102233e
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-04-03 17:23:47 +0530

    Updated P50)Subset-II.cpp

--- Day9_Recursion/P50)Subset-II.cpp
+++ Day9_Recursion/P50)Subset-II.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    void solve(int idx,vector<int>& nums ,vector<int>& ds, vector<vector<int>>& res)
+    {
+      
+        res.push_back(ds);
+          
+        
+        for(int i=idx;i<nums.size();i++)
+        {
+            if(i>idx && nums[i]==nums[i-1])continue;
+            ds.push_back(nums[i]);
+            solve(i+1,nums,ds,res);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+     vector<vector<int>> res;
+     vector<int> ds;
+        sort(nums.begin(),nums.end());
+      solve(0,nums,ds,res);
+      return res;
+    }
+};
+
+
+class Solution {
+public:
     void solve(int idx, vector<int>& nums, vector<int> tmp, set<vector<int>>& ds){
         
         int n = nums.size();
