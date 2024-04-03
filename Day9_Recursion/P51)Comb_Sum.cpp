99d96c0d9b6d3a0fbff12efd997f0da95f434b7c
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-04-03 17:32:04 +0530

    Updated P51)Comb_Sum.cpp

--- Day9_Recursion/P51)Comb_Sum.cpp
+++ Day9_Recursion/P51)Comb_Sum.cpp
@@ -1,4 +1,39 @@
-
+class Solution {
+public:
+    void solve(int idx,int target,vector<int>&nums, vector<int>& ds, 
+               vector<vector<int>>& ans)
+    {
+      if(idx>=nums.size())
+      {
+        if(target==0)
+        {
+            ans.push_back(ds);
+            
+        }
+          return;
+      } 
+        //pick 
+        if(target>=nums[idx])
+        {
+            ds.push_back(nums[idx]);
+            solve(idx,target-nums[idx],nums,ds,ans);
+            ds.pop_back();
+        }
+        
+        // not pick
+        solve(idx+1,target,nums,ds,ans);
+        
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        
+        vector<vector<int>>ans;
+        vector<int> ds;
+        solve(0,target,nums,ds,ans);
+        
+        return ans;
+    }
+};
 class Solution {
 public:
     void solve(int idx,int target,vector<int>&nums, vector<int>tmp ,set<vector<int>>& ds)
