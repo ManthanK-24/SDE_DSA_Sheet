806e64f83ecd5aa1622139e4be9454f72077bd50
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-04-26 14:36:02 +0530

    Updated P44)Min_Platforms.cpp

--- Day8_Greedy/P44)Min_Platforms.cpp
+++ Day8_Greedy/P44)Min_Platforms.cpp
@@ -11,6 +11,7 @@ class Solution{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
+   
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
@@ -35,6 +36,30 @@ class Solution{
     	}
     	return pq.size();
     }
+int countPlatforms(int n,int arr[],int dep[])
+ {
+    sort(arr,arr+n);
+    sort(dep,dep+n);
+ 
+    int ans=1;
+    int count=1;
+    int i=1,j=0;
+    while(i<n && j<n)
+    {
+        if(arr[i]<=dep[j]) //one more platform needed
+        {
+            count++;
+            i++;
+        }
+        else //one platform can be reduced
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count); //updating the value with the current maximum
+    }
+    return ans;
+ }
 };
 
 
