8f89fd8aa50e32e7cde75ead46e8ef0a84ef4ab9
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-05-23 13:43:11 +0530

    Added P78)QueUsingStk.cpp

---
+++ Day13_Stack_Q/P78)QueUsingStk.cpp
@@ -0,0 +1,59 @@
+class MyQueue {
+public:
+    stack<int> input, output;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        if(!output.empty())
+        {
+            int ele = output.top();
+            output.pop();
+            return ele;
+        }
+        else
+        {
+            while(!input.empty())
+            {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int ele = output.top();
+        output.pop();
+        return ele;
+    }
+    
+    int peek() {
+        if(!output.empty())return output.top();
+        else
+        {
+            while(!input.empty())
+            {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int ele = output.top();
+       
+        return ele;
+    }
+    
+    bool empty() {
+        return (output.empty() && input.empty());
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
